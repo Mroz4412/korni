42b1528ef3fa7e73ad46ea15dd8d78f34091196b
Mroz4412 <quba.mroz1@gmail.com>
2025-03-27 20:13:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function () {
+    // Animacja dla tekstu po lewej stronie
+    const loveTextLeft = document.getElementById('loveTextLeft');
+    if (loveTextLeft) {
+        const loveAnimationLeft = new mojs.Html({
+            el: '#loveTextLeft',
+            opacity: { 0: 1 },
+            scale: { 0.5: 1 },
+            duration: 1000,
+            easing: 'cubic.out',
+            delay: 300
+        });
+
+        loveAnimationLeft.play();
+    }
+
+    // Animacja dla tekstu po prawej stronie
+    const loveTextRight = document.getElementById('loveTextRight');
+    if (loveTextRight) {
+        const loveAnimationRight = new mojs.Html({
+            el: '#loveTextRight',
+            opacity: { 0: 1 },
+            scale: { 0.5: 1 },
+            duration: 1500,
+            easing: 'cubic.out',
+            delay: 500
+        });
+
+        loveAnimationRight.play();
+    }
+
     // Funkcja do tworzenia serduszek
     function createHeart() {
         const heart = document.createElement('div');
